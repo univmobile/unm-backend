edb9012b93325248b6a65adf4e1943b2c6339b36
David <david.andriana@avantage-compris.com>
2014-07-23 19:22:46 +0200

    Initial import: Simple test with Selenium.

---
+++ unm-backend-it/src/test/java/fr/univmobile/backend/it/SimpleSeleniumTest.java
@@ -0,0 +1,52 @@
+package fr.univmobile.backend.it;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
+public class SimpleSeleniumTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+		final int seleniumPort = 8888;
+		final String url = "http://localhost:8080/";
+
+		selenium = new DefaultSelenium("localhost", seleniumPort, "*firefox",
+				url);
+
+		selenium.start();
+
+		selenium.open("/unm-backend");
+
+		selenium.waitForPageToLoad(Integer.toString(10000));
+	}
+
+	private Selenium selenium;
+
+	@After
+	public void tearDown() throws Exception {
+
+		selenium.stop();
+	}
+
+	@Test
+	public void testHomePage() throws Exception {
+
+		final String KWARGS = "";
+
+		final File file = new File("target", "home.png");
+
+		selenium.captureEntirePageScreenshot(file.getCanonicalPath(), KWARGS);
+
+		final String pageSource = selenium.getHtmlSource();
+
+		FileUtils.write(new File("target", "pageSource.html"), pageSource);
+	}
+}
