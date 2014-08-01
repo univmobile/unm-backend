1bf6c738780d42811a6f1143286ce6758fe3a6ab
David <david.andriana@avantage-compris.com>
2014-08-01 06:42:06 +0200

    Scenario with Selenium

---
+++ unm-backend-it/src/test/java/fr/univmobile/backend/it/Scenarios001.java
@@ -0,0 +1,49 @@
+package fr.univmobile.backend.it;
+
+import org.junit.Test;
+
+import fr.univmobile.it.commons.DeviceNames;
+import fr.univmobile.it.commons.Scenario;
+import fr.univmobile.it.commons.Scenarios;
+import fr.univmobile.it.commons.SeleniumEnabledTest;
+
+@Scenarios("Scénarios simples")
+@DeviceNames({ "*firefox", "*safari" })
+public class Scenarios001 extends SeleniumEnabledTest {
+
+	private static final int PAUSE = 2000;
+
+	@Scenario("Aller-retour sur la page « Ajouter un utilisateur »")
+	@Test
+	public void sc001() throws Exception {
+
+		takeScreenshot("home.png");
+
+		savePageSource("pageHome.html");
+
+		elementById("button-myself").shouldBeVisible();
+
+		elementById("div-myself")
+				.textShouldContain("Votre connexion est authentifiée");
+
+		elementById("button-myself").click();
+
+		pause(PAUSE);
+
+		takeScreenshot("entered.png");
+
+		/*
+		elementById("link-useradd").click();
+
+		pause(PAUSE);
+
+		takeScreenshot("useradd.png");
+
+		elementById("button-cancel").click();
+
+		pause(PAUSE);
+
+		takeScreenshot("home2.png");
+		*/
+	}
+}
