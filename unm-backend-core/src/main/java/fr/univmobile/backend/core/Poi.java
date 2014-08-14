0def01053f5b2599f997f0f1c65cd1e05d72635c
David <david.andriana@avantage-compris.com>
2014-08-14 22:25:00 +0200

    Start working on Pois (Points of Interest)

---
+++ unm-backend-core/src/main/java/fr/univmobile/backend/core/Poi.java
@@ -0,0 +1,144 @@
+package fr.univmobile.backend.core;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.annotation.Nullable;
+
+import net.avcompris.binding.annotation.XPath;
+
+import org.joda.time.DateTime;
+
+import fr.univmobile.commons.datasource.Entry;
+
+public interface Poi extends Entry {
+
+	/**
+	 * e.g. 1
+	 */
+	@XPath("atom:content/@uid")
+	int getUid();
+
+	/**
+	 * e.g. "Université Panthéon-Sorbonne - Paris I"
+	 */
+	@XPath("atom:content/@name")
+	String getName();
+
+	/**
+	 * e.g. 1 (poiTypeLabel = "Université")
+	 */
+	@XPath("atom:content/atom:poiType/@id")
+	int[] getPoiTypeIds();
+
+	/**
+	 * e.g. "Université"
+	 */
+	@XPath("atom:content/atom:poiType/@label")
+	String[] getPoiTypeLabels();
+
+	/**
+	 * e.g. "/images/universities/logos/univ_paris1.jpg"
+	 */
+	@XPath("atom:content/@logo")
+	String getLogo();
+
+	/**
+	 * e.g. "48.84650925911,2.3459243774"
+	 */
+	@XPath("atom:content/@coordinates")
+	String getCoordinates();
+
+	@XPath("atom:content/@active = 'true'")
+	boolean isActive();
+
+	@XPath("atom:content/@markerType")
+	MarkerType getMarkerType();
+
+	@Nullable
+	@XPath("atom:content/@createdAt")
+	DateTime getCreatedAt();
+
+	@Nullable
+	@XPath("atom:content/@updatedAt")
+	DateTime getUpdatedAt();
+
+	@Nullable
+	@XPath("atom:content/@createdBy")
+	String getCreatedBy();
+
+	@Nullable
+	@XPath("atom:content/@updatedBy")
+	String getUpdatedBy();
+
+	@XPath("atom:content/atom:universities/atom:university")
+	University[] getUniversities();
+
+	int sizeOfUniversities();
+
+	@XPath("atom:content/atom:address/atom:url")
+	String[] getUrls();
+
+	@XPath("atom:content/atom:address/atom:phone")
+	String[] getPhones();
+
+	@XPath("atom:content/atom:address/atom:fax")
+	String[] getFaxes();
+
+	@XPath("atom:content/atom:address/atom:email")
+	String[] getEmails();
+
+	@XPath("atom:content/atom:address")
+	Address[] getAddresses();
+
+	interface Address {
+
+		@XPath("atom:floor")
+		String getFloor();
+
+		@XPath("@zipCode")
+		String getZipCode();
+
+		@XPath("@city")
+		String getCity();
+
+		@XPath("@countryCode")
+		String getCountryCode();
+
+		@XPath("@latitude")
+		String getLatitude();
+
+		@XPath("@longitude")
+		String getLongitude();
+
+		@XPath("atom:fullAddress")
+		String getFullAddress();
+	}
+	
+	@XPath("atom:content/@parentUid")
+	int getParentUid();
+	
+	boolean isNullParentUid();
+
+	@XPath("atom:content/atom:child/@uid")
+	int[] getChildren();
+	
+	int sizeOfChildren();
+	
+	enum MarkerType {
+
+		POINT("point"), POLYGON("polygon"), OVERLAY("overlay");
+
+		private MarkerType(final String label) {
+
+			this.label = checkNotNull(label, "label");
+		}
+
+		public final String label;
+
+		@Override
+		public String toString() {
+
+			return label;
+		}
+	}
+}
---
+++ unm-backend-core/src/test/java/fr/univmobile/backend/core/PoisTest.java
@@ -0,0 +1,85 @@
+package fr.univmobile.backend.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.univmobile.commons.datasource.impl.BackendDataSourceFileSystem;
+
+public class PoisTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+		final File originalDataDir = new File("src/test/data/pois/001");
+
+		final File tmpDataDir = new File("target/PoisTest");
+
+		if (tmpDataDir.isDirectory()) {
+			FileUtils.forceDelete(tmpDataDir);
+		}
+
+		FileUtils.copyDirectory(originalDataDir, tmpDataDir);
+
+		pois = BackendDataSourceFileSystem.newDataSource(PoiDataSource.class,
+				tmpDataDir);
+	}
+
+	private PoiDataSource pois;
+
+	@Test
+	public void test_paris1_Université() throws Exception {
+
+		final Poi paris1 = pois.getByUid(1);
+
+		assertEquals(1, paris1.getPoiTypeIds()[0]);
+		assertEquals("Université", paris1.getPoiTypeLabels()[0]);
+		assertEquals("/images/universities/logos/univ_paris1.jpg",
+				paris1.getLogo());
+		assertEquals("Http://www.univ-paris1.fr/", paris1.getUrls()[0]);
+		assertEquals("0144078000", paris1.getPhones()[0]);
+		assertSame(Poi.MarkerType.POINT, paris1.getMarkerType());
+		assertEquals("48.84650925911,2.3459243774", paris1.getCoordinates());
+		assertTrue(paris1.isActive());
+		assertEquals("12 place du Panthéon 75231 PARIS", 
+				paris1.getAddresses()[0].getFullAddress().trim());
+		assertEquals("75231", paris1.getAddresses()[0].getZipCode());
+		assertEquals("PARIS", paris1.getAddresses()[0].getCity());
+		assertEquals("FR", paris1.getAddresses()[0].getCountryCode());
+		assertEquals("48.85", paris1.getAddresses()[0].getLatitude());
+		assertEquals("2.35", paris1.getAddresses()[0].getLongitude());
+		
+		assertTrue(paris1.isNullParentUid());
+		
+		assertEquals(26, paris1.sizeOfChildren());
+	}
+
+	@Test
+	public void test_uvsq_Université() throws Exception {
+
+		final Poi uvsq = pois.getByUid(4);
+
+		assertEquals(1, uvsq.getPoiTypeIds()[0]);
+		assertEquals("Université", uvsq.getPoiTypeLabels()[0]);
+		assertEquals("Siège : 55 avenue de Parie à Versailles",
+				uvsq.getAddresses()[0].getFloor());
+		assertEquals("01 39 25 78 01", uvsq.getFaxes()[0]);
+		assertEquals("contact@uvsq.fr",uvsq.getEmails()[0]);
+	}
+
+	@Test
+	public void test_paris1_CentreBroca() throws Exception {
+
+		final Poi centreBroca = pois.getByUid(415);
+
+		assertEquals(2, centreBroca.getPoiTypeIds()[0]);
+		assertEquals("Site", centreBroca.getPoiTypeLabels()[0]);
+		assertEquals(1, centreBroca.getParentUid());
+	}
+}
