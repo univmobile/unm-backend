16dffad4a83076b2e8398351c1f0380b10b6b371
David <david.andriana@avantage-compris.com>
2014-08-15 09:37:08 +0200

    Use an abstract XxxPoisTest.java test class to check coherence between hand-crafted and generated XML POI files

--- unm-backend-core/src/test/java/fr/univmobile/backend/core/AbstractPoisTest.java
+++ unm-backend-core/src/test/java/fr/univmobile/backend/core/AbstractPoisTest.java
@@ -12,12 +12,21 @@ import org.junit.Test;
 
 import fr.univmobile.commons.datasource.impl.BackendDataSourceFileSystem;
 
-public class PoisTest {
+public abstract class AbstractPoisTest {
+
+	protected AbstractPoisTest(final File originalDataDir) {
+
+		this.originalDataDir = originalDataDir;
+	}
+
+	private final File originalDataDir;
 
 	@Before
-	public void setUp() throws Exception {
+	public final void setUp() throws Exception {
 
-		final File originalDataDir = new File("src/test/data/pois/001");
+		if (pois != null) { // Load data only once
+			return;
+		}
 
 		final File tmpDataDir = new File("target/PoisTest");
 
@@ -27,19 +36,25 @@ public class PoisTest {
 
 		FileUtils.copyDirectory(originalDataDir, tmpDataDir);
 
+		System.out.println("Loading data...");
+		
 		pois = BackendDataSourceFileSystem.newDataSource(PoiDataSource.class,
 				tmpDataDir);
+		
+		System.out.println("Loadind data: Done.");		
 	}
 
-	private PoiDataSource pois;
+	private static PoiDataSource pois;
 
 	@Test
-	public void test_paris1_Université() throws Exception {
+	public final void test_paris1_University() throws Exception {
 
 		final Poi paris1 = pois.getByUid(1);
 
 		assertEquals(1, paris1.getPoiTypeIds()[0]);
 		assertEquals("Université", paris1.getPoiTypeLabels()[0]);
+		assertEquals(1, paris1.getPoiCategoryIds()[0]);
+		assertEquals("Plans", paris1.getPoiCategoryLabels()[0]);
 		assertEquals("/images/universities/logos/univ_paris1.jpg",
 				paris1.getLogo());
 		assertEquals("Http://www.univ-paris1.fr/", paris1.getUrls()[0]);
@@ -47,21 +62,22 @@ public class PoisTest {
 		assertSame(Poi.MarkerType.POINT, paris1.getMarkerType());
 		assertEquals("48.84650925911,2.3459243774", paris1.getCoordinates());
 		assertTrue(paris1.isActive());
-		assertEquals("12 place du Panthéon 75231 PARIS", 
+		assertEquals("12 place du Panthéon 75231 PARIS",
 				paris1.getAddresses()[0].getFullAddress().trim());
 		assertEquals("75231", paris1.getAddresses()[0].getZipCode());
 		assertEquals("PARIS", paris1.getAddresses()[0].getCity());
 		assertEquals("FR", paris1.getAddresses()[0].getCountryCode());
 		assertEquals("48.85", paris1.getAddresses()[0].getLatitude());
 		assertEquals("2.35", paris1.getAddresses()[0].getLongitude());
-		
+		assertEquals("paris1", paris1.getUniversities()[0]);
+
 		assertTrue(paris1.isNullParentUid());
-		
+
 		assertEquals(26, paris1.sizeOfChildren());
 	}
 
 	@Test
-	public void test_uvsq_Université() throws Exception {
+	public final void test_uvsq_University() throws Exception {
 
 		final Poi uvsq = pois.getByUid(4);
 
@@ -70,11 +86,11 @@ public class PoisTest {
 		assertEquals("Siège : 55 avenue de Parie à Versailles",
 				uvsq.getAddresses()[0].getFloor());
 		assertEquals("01 39 25 78 01", uvsq.getFaxes()[0]);
-		assertEquals("contact@uvsq.fr",uvsq.getEmails()[0]);
+		assertEquals("contact@uvsq.fr", uvsq.getEmails()[0]);
 	}
 
 	@Test
-	public void test_paris1_CentreBroca() throws Exception {
+	public final void test_paris1_CentreBroca() throws Exception {
 
 		final Poi centreBroca = pois.getByUid(415);
 
---
+++ unm-backend-core/src/test/java/fr/univmobile/backend/core/Pois001Test.java
@@ -0,0 +1,11 @@
+package fr.univmobile.backend.core;
+
+import java.io.File;
+
+public class Pois001Test extends AbstractPoisTest {
+
+	public Pois001Test() {
+
+		super(new File("src/test/data/pois/001"));
+	}
+}
