c283fa6ce28391545d447839af7beb34253e655b
David <david.andriana@avantage-compris.com>
2014-08-15 09:39:00 +0200

    BIG IMPORT (30 Mo) — Add XML POI files generated from MySQL source (Géocampus)

---
+++ unm-backend-core/src/test/java/fr/univmobile/backend/core/Pois002Test.java
@@ -0,0 +1,11 @@
+package fr.univmobile.backend.core;
+
+import java.io.File;
+
+public class Pois002Test extends AbstractPoisTest {
+
+	public Pois002Test() {
+
+		super(new File("src/test/data/pois/002"));
+	}
+}
