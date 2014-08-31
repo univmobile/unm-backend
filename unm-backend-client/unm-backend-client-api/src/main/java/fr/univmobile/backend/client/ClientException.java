04f0a0345d85764b6ebc3882f00ccb49b49f79d0
David <david.andriana@avantage-compris.com>
2014-08-31 08:00:51 +0200

    Add exception class: ClientException

---
+++ unm-backend-client/unm-backend-client-api/src/main/java/fr/univmobile/backend/client/ClientException.java
@@ -0,0 +1,20 @@
+package fr.univmobile.backend.client;
+
+import javax.annotation.Nullable;
+
+/**
+ * Exception class of the client layer. The client layer is in the application
+ * layer.
+ */
+public class ClientException extends Exception {
+
+	/**
+	 * for serialization.
+	 */
+	private static final long serialVersionUID = 2589556862838545813L;
+
+	public ClientException(@Nullable final String message) {
+
+		super(message);
+	}
+}
