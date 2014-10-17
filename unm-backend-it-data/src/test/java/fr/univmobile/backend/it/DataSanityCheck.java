9a1e008460fc498a6384acf0bc6b04177aad01da
David <david.andriana@avantage-compris.com>
2014-10-17 16:53:35 +0200

    Add data sanity checks: User.primaryUniversity must be set

---
+++ unm-backend-it-data/src/test/java/fr/univmobile/backend/it/DataSanityCheck.java
@@ -0,0 +1,56 @@
+package fr.univmobile.backend.it;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import fr.univmobile.backend.sanitycheck.BackendChecker;
+
+@RunWith(Parameterized.class)
+public class DataSanityCheck {
+
+	@Parameters(name = "{0}")
+	public static Collection<Object[]> parameters() throws IOException {
+
+		final Collection<Object[]> parameters = new ArrayList<Object[]>();
+
+		for (final File dir : new File("src/main/resources/data").listFiles()) {
+
+			if (!dir.isDirectory()) {
+				continue;
+			}
+
+			parameters.add(new Object[] { "dir:" + dir.getName(), dir });
+		}
+
+		return parameters;
+	}
+
+	public DataSanityCheck(final String dirLabel, final File dataDir) {
+
+		this.dataDir = dataDir;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+		checker = new BackendChecker();
+	}
+
+	private BackendChecker checker;
+
+	private final File dataDir;
+
+	@Test
+	public void testData() throws Exception {
+
+		checker.checkDirectories(dataDir);
+	}
+}
