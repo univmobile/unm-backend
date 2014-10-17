ec597fa862c6af08ce85dfadf7dac2c224221131
David <david.andriana@avantage-compris.com>
2014-10-17 16:52:03 +0200

    Refactor: Add a test suite for each dataDir

--- unm-backend-sysadmin/src/test/java/fr/univmobile/backend/sysadmin/data/DataSanityCheck.java
+++ unm-backend-sysadmin/src/test/java/fr/univmobile/backend/sysadmin/data/DataSanityCheck.java
@@ -1,32 +1,56 @@
 package fr.univmobile.backend.sysadmin.data;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import fr.univmobile.backend.sanitycheck.BackendChecker;
 
+@RunWith(Parameterized.class)
 public class DataSanityCheck {
 
-	@Before
-	public void setUp() throws Exception {
-		
-		checker = new BackendChecker();
-	}
-	
-	private BackendChecker checker; 
-			
-	@Test
-	public void testUsersData() throws Exception {
-		
-		for (final File dir:new File("src/test/data").listFiles()) {
-			
+	@Parameters(name = "{0}")
+	public static Collection<Object[]> parameters() throws IOException {
+
+		final Collection<Object[]> parameters = new ArrayList<Object[]>();
+
+		for (final File dir : new File("src/test/data").listFiles()) {
+
 			if (!dir.isDirectory()) {
 				continue;
 			}
-			
-			checker.checkDirectories(dir);
+
+			parameters.add(new Object[] { "dir:" + dir.getName(), dir });
 		}
+
+		return parameters;
+	}
+
+	public DataSanityCheck(final String dirLabel, final File dataDir) {
+
+		this.dataDir = dataDir;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+		checker = new BackendChecker();
+	}
+
+	private BackendChecker checker;
+
+	private final File dataDir;
+
+	@Test
+	public void testData() throws Exception {
+
+		checker.checkDirectories(dataDir);
 	}
 }
