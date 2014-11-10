eda42f9cee91c5bb7bcddd63e785fa15a5ed5788
nicoviewniverse <nicolas.hoppenot@viewniverse-studio.com>
2014-11-10 10:18:55 +0200

    Add in the classpath the missing net.avcompris libraries

--- unm-backend-sysadmin/index.sh
+++ unm-backend-sysadmin/index.sh
@@ -3,13 +3,13 @@
 M2_REPO="${HOME}/.m2/repository"
 
 CP=
-CP="${CP}:${HOME}/Documents/workspace/unm-backend/unm-backend-sysadmin/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/unm-backend/unm-backend-core/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/unm-backend/unm-commons-datasource/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/avc-binding-dom/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/avc-binding-common/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/avc-binding-yaml/target/classes"
-CP="${CP}:${HOME}/Documents/workspace/avc-commons-lang/target/classes"
+CP="${CP}:${HOME}/project/unm-backend/unm-backend-sysadmin/target/classes"
+CP="${CP}:${HOME}/project/unm-backend/unm-backend-core/target/classes"
+CP="${CP}:${HOME}/project/unm-backend/unm-commons-datasource/target/classes"
+CP="${CP}:${HOME}/project/avc-binding-dom/target/classes"
+CP="${CP}:${HOME}/project/avc-binding-common/target/classes"
+CP="${CP}:${HOME}/project/avc-binding-yaml/target/classes"
+CP="${CP}:${HOME}/project/avc-commons-lang/target/classes"
 CP="${CP}:${M2_REPO}/com/google/guava/guava/14.0.1/guava-14.0.1.jar"
 CP="${CP}:${M2_REPO}/commons-io/commons-io/2.4/commons-io-2.4.jar"
 CP="${CP}:${M2_REPO}/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar"
@@ -24,7 +24,11 @@ CP="${CP}:${M2_REPO}/log4j/log4j/1.2.17/log4j-1.2.17.jar"
 CP="${CP}:${M2_REPO}/jvyaml/jvyaml/0.2.1/jvyaml-0.2.1.jar"
 CP="${CP}:${M2_REPO}/xml-apis/xml-apis/1.4.01/xml-apis-1.4.01.jar"
 CP="${CP}:${M2_REPO}/org/yaml/snakeyaml/1.12/snakeyaml-1.12.jar"
+CP="${CP}:${M2_REPO}/net/avcompris/commons/avc-commons-lang/0.4.3/avc-commons-lang-0.4.3.jar"
+CP="${CP}:${M2_REPO}/net/avcompris/commons/avc-binding-yaml/0.1.1/avc-binding-yaml-0.1.1.jar"
+CP="${CP}:${M2_REPO}/net/avcompris/commons/avc-binding-common/0.1.4/avc-binding-common-0.1.4.jar"
+CP="${CP}:${M2_REPO}/net/avcompris/commons/avc-binding-dom/0.1.6/avc-binding-dom-0.1.6.jar"
 
 java -cp "${CP}" fr.univmobile.backend.sysadmin.Main index \
-	-data src/test/data/001/ -dburl jdbc:mysql://localhost/toto \
+	-data /var/univmobile -dburl jdbc:mysql://localhost/univmobile \
 	-dbusername root -dbpassword $@
