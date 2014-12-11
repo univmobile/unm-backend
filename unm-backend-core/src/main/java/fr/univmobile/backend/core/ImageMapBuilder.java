e4f60f9c4edf884447756adc21115f3cf9d815d4
lviewniverse <lviewniverse@users.noreply.github.com>
2014-12-11 01:25:08 -0300

    Merged with develop head

--- unm-backend-core/src/main/java/fr/univmobile/backend/core/ImageMapBuilder.java
+++ unm-backend-core/src/main/java/fr/univmobile/backend/core/ImageMapBuilder.java
@@ -5,7 +5,18 @@ import fr.univmobile.commons.datasource.EntryBuilder;
 public interface ImageMapBuilder extends EntryBuilder<ImageMap>, ImageMap {
 
 	ImageMapBuilder setUid(int uid);
-	
+
 	ImageMapBuilder setName(String name);
 
+	// Author: Mauricio (begin)
+
+	ImageMapBuilder setActive(String active);
+
+	ImageMapBuilder setImageUrl(String url);
+
+	ImageMapBuilder setDescription(String description);
+
+	ImageMapBuilder setPoiInfos(String poisInfos);
+
+	// Author: Mauricio (end)
 }
