0457290b89974624d2d81df66ba5482da3e0b302
murraco <mauriurraco@gmail.com>
2015-01-20 23:27:02 -0300

    Solved dependencies problems in unm-backend-app.
    Now the spring-aspect dependency version of unm-backend-domain is the
    same version that in unm-backend-app.

--- unm-backend-domain/src/main/java/fr/univmobile/backend/domain/MenuRepository.java
+++ unm-backend-domain/src/main/java/fr/univmobile/backend/domain/MenuRepository.java
@@ -1,10 +1,10 @@
 package fr.univmobile.backend.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "menu", path = "menues")
 public interface MenuRepository extends JpaRepository<Menu, Long> {
