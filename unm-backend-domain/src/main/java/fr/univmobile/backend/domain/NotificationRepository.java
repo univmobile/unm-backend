b2dc6e62bd811c49ffce015ed597c9ef52a21891
lviewniverse <lviewniverse@users.noreply.github.com>
2015-01-29 02:52:56 -0300

    Added some notification search methods for API serving using SDR

--- unm-backend-domain/src/main/java/fr/univmobile/backend/domain/NotificationRepository.java
+++ unm-backend-domain/src/main/java/fr/univmobile/backend/domain/NotificationRepository.java
@@ -1,8 +1,22 @@
 package fr.univmobile.backend.domain;
 
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 
 public interface NotificationRepository extends	JpaRepository<Notification, Long>, NotificationRepositoryCustom {
+	
+	@Query("Select n from Notification n where (n.university = :universityId or n.university = null) order by n.createdOn desc")
+	Page<News> findNotificationsForUniversity(@Param("universityId") University universityId, Pageable pageable);
+	
+	@Query("Select n from Notification n where (n.university = :universityId or n.university = null) and n.createdOn > :since order by n.createdOn desc")
+	Page<News> findNotificationsForUniversitySince(@Param("universityId") University universityId, @Param("since") @DateTimeFormat(iso = ISO.DATE_TIME) Date since, Pageable pageable);
 
 }
