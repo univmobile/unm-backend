22a485434877ec92f8517716d1217d13b29c422b
nicoviewniverse <nicolas.hoppenot@viewniverse-studio.com>
2015-04-09 19:20:34 +0300

    Add a method to get only the universities by region

--- unm-backend-domain/src/main/java/fr/univmobile/backend/domain/UniversityRepository.java
+++ unm-backend-domain/src/main/java/fr/univmobile/backend/domain/UniversityRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostFilter;
 
 public interface UniversityRepository extends JpaRepository<University, Long> {
@@ -34,6 +35,9 @@ public interface UniversityRepository extends JpaRepository<University, Long> {
 	/** Get all the universities of type CROUS */
 	@Query("select u from University u where u.crous = TRUE order by u.region.id, u.title")
 	List<University> getCrous();
+	
+	@Query("select u from University u where u.active = TRUE and u.crous = FALSE and u.region.id = :regionId order by u.title")
+	List<University> findAllActiveWithoutCrousByRegion(@Param("regionId") Long regionId);
 
 	List<University> findAllByRegion_Id(Long regionId);
 
