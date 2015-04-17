769e73a6da9dcf00e8ad5305b12e8cfb5a1a8c73
lviewniverse <lviewniverse@users.noreply.github.com>
2015-04-16 22:19:45 -0300

    Changed @RestResource for @RepositoryRestResource to allow customizations for SDR the documented way.

--- unm-backend-domain/src/main/java/fr/univmobile/backend/domain/TokenRepository.java
+++ unm-backend-domain/src/main/java/fr/univmobile/backend/domain/TokenRepository.java
@@ -1,9 +1,9 @@
 package fr.univmobile.backend.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RestResource(exported = false)
+@RepositoryRestResource(exported = false)
 public interface TokenRepository extends JpaRepository<Token, Long> {
     public Token findByToken(String token);
 }
